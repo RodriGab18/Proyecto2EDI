0db4a77fc44b2437b72df43364313a5daf2d84c5
RodriGab18 <lunguita100000@gmail.com>
2025-03-19 19:17:33 -0600

    Inicio de proyecto
    Se crearon los primeros archivos del proyecto con un poco de contenido.

---
+++ NodoCancion.py
@@ -0,0 +1,6 @@
+class NodoCancion:
+    def __init__(self, nombreCancion, artista, duracion, rutaArchivo):
+        self.nombreCancion = nombreCancion
+        self.artista = artista
+        self.duracion = duracion
+        self.rutaArchivo = rutaArchivo
